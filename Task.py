87dcef5695bd57a8014204b68b82b4eab1cb2056
Omer Caspi <omer.caspi@gmail.com>
2021-10-02 01:41:31 +0300

    tr: initial commit
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

---
+++ Task.py
@@ -0,0 +1,206 @@
+from common import *
+from config import *
+from argparse import Namespace as Args
+import shlex
+import textwrap
+import subprocess
+
+
+class Task(object):
+    def __init__(self, task: dict, name: str, config: Config, args: Args) -> None:
+        super().__init__()
+        self._task = task
+        self.name = name
+        self.config = config
+        self.args = args
+        self.short_desc = task.get(TaskKeys.SHORT_DESC, None)
+        self.long_desc = task.get(TaskKeys.LONG_DESC, None)
+
+        if args.stop_on_error:
+            self.stop_on_error = args.stop_on_error
+        else:
+            self.stop_on_error = task.get(TaskKeys.STOP_ON_ERROR, True)
+
+        if args.command:
+            self.commands = args.command
+        elif args.command_append:
+            self.commands += args.append_command
+        else:
+            self.commands = task.get(TaskKeys.COMMANDS, [])
+
+        if args.cwd:
+            self.cwd = args.cwd
+        else:
+            self.cwd = task.get(TaskKeys.CWD, None)
+
+        if args.shell:
+            self.shell = args.shell
+        else:
+            self.shell = task.get(TaskKeys.SHELL, False)
+
+        if self.shell:
+            if args.shell_path:
+                self.shell_path = args.shell_path
+            else:
+                self.shell_path = task.get(TaskKeys.SHELL_PATH, config.default_shell_path())
+        else:
+            self.shell_path = None
+
+        cli_env = []
+        if args.env:
+            cli_env = args.env
+            self.env = {}
+        else:
+            self.env = task.get(TaskKeys.ENV, {})
+            if args.env_append:
+                cli_env = args.env_append
+
+        for e in cli_env:
+            e_name, e_value = Task._parse_assignmet_str(e)
+            self.env[e_name] = e_value
+
+        if args.image:
+            self.image = args.image
+        else:
+            self.image = task.get(TaskKeys.IMAGE, None)
+
+        if not self.image:
+            return
+
+        #  Container specific settings
+        if args.volumes:
+            self.volumes = args.volumes
+        else:
+            self.volumes = task.get(TaskKeys.VOLUMES, [])
+            if args.volumes_append:
+                self.volumes += args.volumes_append
+
+        if args.container_tool:
+            self.container_tool = args.container_tool
+        else:
+            self.container_tool = self.config.default_container_tool()
+
+    def _container_execute(self, command: str) -> int:
+        cmd_array = [self.container_tool, "run", "-it", "--rm"]
+        if self.cwd:
+            cmd_array += ["-w", self.cwd]
+
+        for v in self.volumes:
+            cmd_array += ["-v", expand_string(v, self.config.defs)]
+
+        cmd_array.append(self.image)
+        if self.shell:
+            shell = "/usr/bin/sh" if self.shell_path is None else self.shell_path
+            command = "\"{}\"".format(command)
+            cmd_array += [shell, "-c", "\"{}\"".format(command)]
+        else:
+            cmd_array += [command]
+        print(cmd_array)
+        p = subprocess.Popen(cmd_array, stdout=sys.stdout, stderr=sys.stderr)
+        return p.wait()
+
+    @staticmethod
+    def _parse_assignmet_str(s: str):
+        parts = s.split('=', maxsplit=1)
+        if len(parts) == 1:
+            return s, ""
+        return parts[0], str(parts[1])
+
+    def _system_execute(self, cmd_str: str) -> int:
+        if self.shell:
+            cmd_array = [cmd_str]
+        else:
+            try:
+                cmd_array = shlex.split(cmd_str)
+            except ValueError as e:
+                raise TaskException("Illegal command '{}' for task '{}' - {}".format(
+                    cmd_str, self.name, e))
+        try:
+            if self.env is None:
+                penv = None
+            else:
+                #  penv = os.environ.copy()
+                penv = {}
+                penv.update(self.env)
+            p = subprocess.Popen(cmd_array, shell=self.shell, executable=self.shell_path,
+                                 stdout=sys.stdout, stderr=sys.stderr, env=penv, cwd=self.cwd)
+            return p.wait()
+
+        except (OSError, FileNotFoundError) as e:
+            raise TaskException("Error occured running command '{}' - {}".format(cmd_str, e))
+
+    def execute(self) -> int:
+        if not self.image and len(self.commands) == 0:
+            print("No commands defined for task '{}'. Nothing to do.".format(self.name))
+            return 0
+
+        rc = 0
+        for cmd in self.commands:
+            cmd_str = expand_string(cmd, self.config.defs)
+            if self.image:
+                cmd_rc = self._container_execute(cmd_str)
+            else:
+                cmd_rc = self._system_execute(cmd_str)
+            if cmd_rc == 0:
+                continue
+            if self.stop_on_error:
+                return cmd_rc
+            if rc == 0:
+                rc = cmd_rc
+        return rc
+
+    def show_info(self):
+        PRINT_FMT = "{:<24}{:<80}"
+
+        def print_val(title: str, value: typing.Any):
+            print(PRINT_FMT.format("{}".format(title), value))
+
+        def print_bool(title, value: bool):
+            print_val(title, "Yes" if value else "No")
+
+        def print_blob(title: str, text: str):
+            if text is None:
+                text = ""
+            first = True
+            for line in text.split('\n'):
+                for in_line in textwrap.wrap(line, width=80):
+                    if first:
+                        print_val(title, in_line)
+                        first = False
+                        continue
+                    print(PRINT_FMT.format("", in_line))
+
+        print(PRINT_FMT.format("Task name:", self.name))
+        print_val("Short description:", self.short_desc)
+        if self.long_desc:
+            print_blob("Description:", self.long_desc)
+
+        if self.image:
+            print_val("Image:", self.image)
+
+        if self.shell:
+            if self.shell_path:
+                print_val("Shell:", self.shell_path)
+            else:
+                print_val("Shell:", "default (/usr/bin/sh)")
+        print_bool("Stop on error:", self.stop_on_error)
+
+        count = 0
+        if self.env:
+            print(PRINT_FMT.format("Environment:", ""))
+            for k, v in self.env.items():
+                print_blob("     [{}]".format(count), "{}={}".format(k, v))
+                count += 1
+
+        if len(self.commands) == 0:
+            print("NOTICE: No commands defined for task")
+            return
+        if len(self.commands) == 1:
+            print_blob("Command:", expand_string(self.commands[0], self.config.defs))
+            return
+
+        print(PRINT_FMT.format("Commands:", ""))
+        count = 0
+        for cmd_str in self.commands:
+            print_blob("     [{}]".format(count), expand_string(cmd_str, self.config.defs))
+            count += 1
---
+++ config.py
@@ -0,0 +1,227 @@
+from common import *
+import os
+import pathlib
+import json
+import jsonschema
+
+
+def print_dict(d: dict):
+    print(json.dumps(d, indent=4))
+
+
+class _GlobalKeys(object):
+    VERSION = "version"
+    TASKS_KEY = "tasks"
+    DEFS_KEY = "definitions"
+    INCLUDE_KEY = "include"
+    DEFAULT_TASK = "default_task"
+    SUPRESS_KEY = "supress"
+    DEFAULT_SHELL_PATH = "defult_shell_path"
+    DEFAULT_CONTAINER_TOOL = "default_container_tool"
+
+
+class TaskKeys(object):
+    VERSION = "version"
+    SHORT_DESC = "short_desc"
+    LONG_DESC = "description"
+    COMMANDS = "commands"
+    SHELL = "shell"
+    SHELL_PATH = "shell_path"
+    STOP_ON_ERROR = "stop_on_error"
+    ENV = "env"
+    IMAGE = "image"
+    CONTAINER_TOOL = "container_tool"
+    CWD = "cwd"
+    VOLUMES = "volumes"
+
+
+class AutoDefsKyes(object):
+    # Auto defintiosn keys
+    CONF_PATH = "CONF_PATH"
+    CWD = "CWD"
+    CWD_REL_CONF = "CWD_REL_CONF"
+
+
+class Config(object):
+    _CONF_FILE_NAME = "task_runner.json"
+    _MAJOR_VER: int = 0
+    _MINOR_VER: int = 1
+
+    _TASK_SCHEMA = {
+        "type": "object",
+        "properties": {
+            TaskKeys.SHORT_DESC: {"type": "string", "maxLength": 75},
+            TaskKeys.LONG_DESC: {"type": "string"},
+            TaskKeys.COMMANDS: {
+                "type": "array",
+                "items": {"type": "string", "minLength": 1}
+            },
+            TaskKeys.CWD: {"type": "string", "maxLength": 1},
+            TaskKeys.SHELL: {"type": "boolean"},
+            TaskKeys.SHELL_PATH: {"type": "string", "minLength": 1},
+            TaskKeys.ENV: {
+                "type": "object",
+                "additionalProperties": {
+                    "type": "string"
+                }
+            },
+            TaskKeys.IMAGE: {"type": "string", "maxLength": 64},
+            TaskKeys.CONTAINER_TOOL: {"type": "string"},
+            TaskKeys.VOLUMES: {
+                "type": "array",
+                "items": {"type": "string"}
+            }
+        }
+    }
+    _CONFIG_SCHEMA = {
+        TaskKeys.VERSION: {
+            "type": "object",
+            "properties": {
+                "major": {"type": "number", "minValue": 0},
+                "minor": {"type": "number", "minValue": 1}
+            }
+        },
+        "type": "object",
+        "properties": {
+            _GlobalKeys.DEFS_KEY: {"type": "object"},
+            _GlobalKeys.DEFAULT_TASK: {"type": "string"},
+            _GlobalKeys.TASKS_KEY: {"type": "object"},
+            _GlobalKeys.SUPRESS_KEY: {
+                "type": "array",
+                "items": {
+                    "type": "string",
+                    "minLength": 1
+                }
+            },
+            _GlobalKeys.DEFAULT_SHELL_PATH: {
+                "type": "string",
+                "minLength": 1
+            }
+        },
+        "required": [TaskKeys.VERSION]
+    }
+
+    @staticmethod
+    def _read_tasks_file(file_path: str) -> dict:
+        try:
+            data: dict = json.load(open(file_path, 'r'))
+            jsonschema.validate(data, schema=Config._CONFIG_SCHEMA)
+            return data
+        except (IOError, TypeError, ValueError, jsonschema.ValidationError) as e:
+            raise TaskException("Error parsing {} - {}".format(file_path, e))
+
+    @staticmethod
+    def _check_config_file_version(data: dict, local: bool) -> None:
+        conf_file_type = "local" if local else "global"
+        major = data["version"]["major"]
+        minor = data["version"]["minor"]
+        if major != Config._MAJOR_VER or minor > Config._MINOR_VER:
+            raise TaskException(("Incompatible {} configuration file version: " +
+                    "Found:{}.{}, expected <= {}.{}").format(
+                conf_file_type, major, minor, Config._MAJOR_VER, Config._MINOR_VER))
+        if minor != Config._MINOR_VER:
+            print("Incompatible {} configuration file minor version".format(conf_file_type))
+
+    def _read_local_conf_file(self) -> typing.Tuple[dict, str]:
+        directory = os.getcwd()
+        f = directory + "/" + Config._CONF_FILE_NAME
+        while not os.path.exists(f):
+            if directory == "/":
+                return {}, ""
+            directory = os.path.dirname(directory)
+            f = directory + "/" + Config._CONF_FILE_NAME
+        data = Config._read_tasks_file(f)
+        Config._check_config_file_version(data, local=True)
+        return data, f
+
+    def _read_global_conf_file(self):
+        f = str(pathlib.Path.home()) + "/.config/" + Config._CONF_FILE_NAME
+        if not os.path.isfile(f):
+            return {}, ""
+        data = Config._read_tasks_file(f)
+        Config._check_config_file_version(data, local=False)
+        return data, f
+
+    def __init__(self):
+        self.global_conf, self.global_conf_path = self._read_global_conf_file()
+        self.local_conf, self.local_conf_path = self._read_local_conf_file()
+
+        self.global_tasks = self.global_conf.get(_GlobalKeys.TASKS_KEY, {})
+        self.local_tasks = self.local_conf.get(_GlobalKeys.TASKS_KEY, {})
+
+        self.defs = self.global_conf.get(_GlobalKeys.DEFS_KEY, {})
+        self.defs.update(self.local_conf.get(_GlobalKeys.DEFS_KEY, {}))
+
+        self.defs[AutoDefsKyes.CWD] = os.getcwd()
+        if self.local_conf:
+            self.defs[AutoDefsKyes.CONF_PATH] = os.path.dirname(self.local_conf_path)
+            self.defs[AutoDefsKyes.CWD_REL_CONF] = os.path.relpath(
+                    self.defs[AutoDefsKyes.CWD], self.defs[AutoDefsKyes.CONF_PATH])
+
+        self.supressed_tasks = self.global_conf.get(_GlobalKeys.SUPRESS_KEY, [])
+        self.supressed_tasks += self.local_conf.get(_GlobalKeys.SUPRESS_KEY, [])
+
+    def local_setting(self, path: str):
+        return json_dict_value(self.local_conf, path)
+
+    def global_setting(self, path: str):
+        return json_dict_value(self.global_conf, path)
+
+    def default_task_name(self) -> typing.Union[str, None]:
+        return self.setting(_GlobalKeys.DEFAULT_TASK)
+
+    def default_container_tool(self) -> typing.Union[str, None]:
+        return self.setting(_GlobalKeys.DEFAULT_CONTAINER_TOOL, default="/usr/bin/docker")
+
+    def default_shell_path(self) -> typing.Union[str, None]:
+        return self.setting(_GlobalKeys.DEFAULT_SHELL_PATH)
+
+    #  Return anything. Types is forced by schema validations.
+    def setting(self, path: str, default=None) -> typing.Any:
+        setting = self.local_setting(path)
+        if setting is not None:
+            return setting
+        if setting is not None:
+            self.global_setting(path)
+        return default
+
+    def _raw_task(self, name: str) -> typing.Any:
+        if name is None:
+            raise TaskException("No task name was given")
+        try:
+            if name.startswith("global/"):
+                task = self.global_tasks[name[7:]]
+            else:
+                task = self.local_tasks[name]
+        except KeyError:
+            return None
+        return task
+
+    def _include(self, task: dict, to_include: list, included_list: list):
+        for t_name in to_include:
+            if t_name in included_list:
+                continue
+            t = self._raw_task(t_name)
+            if t is None:
+                raise TaskException("Unknown included task '{}'".format(t_name))
+            included_list.append(t_name)
+            self._include(t, t.get(_GlobalKeys.INCLUDE_KEY, []), included_list)
+            task.update(t)
+
+    def task(self, name: str) -> dict:
+        main_task = self.local_tasks.get(name, self.global_tasks.get(name, None))
+        if main_task is None or name in self.supressed_tasks:
+            raise TaskException("No such task '{}'".format(name))
+
+        task = {}
+        try:
+            self._include(task, main_task.get(_GlobalKeys.INCLUDE_KEY, []), [])
+        except TaskException as e:
+            raise TaskException("Error parsing task '{}' - {}".format(name, e))
+        task.update(main_task)
+        try:
+            jsonschema.validate(task, schema=Config._TASK_SCHEMA)
+        except (jsonschema.ValidationError) as e:
+            raise TaskException("Error parsing task '{}' - {}".format(name, e))
+        task.pop(_GlobalKeys.INCLUDE_KEY, None)
+        return task
