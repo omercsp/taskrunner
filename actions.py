87875ca8b14c348d20a9dc643add15393b7e0d8b
Omer Caspi <omer.caspi@gmail.com>
2022-01-17 23:01:27 +0200

    tr: names only task list
    Add a (help suppressed) flag to list the tasks as a space separated names
    only. Can be used for specific bash completion functionality (with
    aliases and such).
    
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

--- actions.py
+++ actions.py
@@ -111,7 +111,7 @@ def show_task_info(args: Args, config: Config) -> None:
     _show_task(task, config, True)
 
 
-def list_tasks(config: Config, show_all: bool):
+def list_tasks(config: Config, show_all: bool, names_only:bool):
     PRINT_FMT = "{:<24}{:<6}{:<55}"
     default_task_name = config.default_task_name()
     local_tasks = config.local_tasks.keys()
@@ -119,6 +119,9 @@ def list_tasks(config: Config, show_all: bool):
     def print_task(name: str, task: dict):
         t = Task(task, name, config=config)
         if not show_all and t.hidden:
+            return
+        if names_only:
+            print(task_name, end=' ')
             return
         desc = "" if t.short_desc is None else t.short_desc
         if len(desc) > 55:
@@ -133,8 +136,9 @@ def list_tasks(config: Config, show_all: bool):
 
         print(PRINT_FMT.format(task_name, flags, desc[-55:]))
 
-    print(PRINT_FMT.format("Name", "Flags", "Description"))
-    print(PRINT_FMT.format("----", "-----", "-----------"))
+    if not names_only:
+	print(PRINT_FMT.format("Name", "Flags", "Description"))
+	print(PRINT_FMT.format("----", "-----", "-----------"))
     for task_name in local_tasks:
         print_task(task_name, config.task_descriptor(task_name))
     if not config.setting(ConfigSchema.Keys.AllowGlobal, True):
