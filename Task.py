617477f28bf6fda4c6c91a61bfb6b6cf80ff7900
Omer Caspi <omer.caspi@gmail.com>
2022-04-20 15:33:57 +0300

    tr: variables expansion as a separate step
    Expand task settings only explicitly, and in a single place. Somewhat
    simplify the somewhat complex task running code, eliminates differences
    between task info and task run, if there is any, as all expansions occur
    in the same place.
    
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

--- Task.py
+++ Task.py
@@ -46,6 +46,8 @@ class Task(object):
         self.c_env = task_descriptor.get(Schema.Keys.Task.CEnv, {})
         self.c_sudo = task_descriptor.get(Schema.Keys.Task.CSudo, False)
 
+        self.expanded = False
+
     def args_update(self, args: Args) -> None:
         if args.stop_on_error:
             self.stop_on_error = args.stop_on_error
@@ -91,6 +93,28 @@ class Task(object):
                 e_name, e_value = parse_assignment_str(e)
                 self.c_env[e_name] = e_value
 
+    def expand_args(self, expander: StringVarExpander) -> None:
+        if self.expanded:
+            warn("Task '{}' is already expanded", self.name)
+            return
+        info("Expanding task '{}'".format(self.name))
+        self.expanded = True
+        if self.env is not None:
+            self.env = {expander(k): expander(v) for k, v in self.env.items()}
+            for k, v in self.env.items():
+                info("Environment variable will be set as '{}={}'", k, v)
+        if self.cwd:
+            self.cwd = expander(self.cwd)
+        self.commands = [expander(c) for c in self.commands]
+
+        if self.c_cwd:
+            self.c_cwd = expander(self.c_cwd)
+        if self.c_image:
+            self.c_image = expander(self.c_image)
+        self.c_volumes = [expander(v) for v in self.c_volumes]
+        self.c_env = {expander(k): expander(v) for k, v in self.c_env.items()}
+
+
     def _simple_cmd_arr(self, cmd) -> list:
         info("Preparing simple command")
         if self.shell:
@@ -100,12 +124,12 @@ class Task(object):
         except ValueError as e:
             raise TaskException("Illegal command '{}' for task '{}' - {}".format(cmd, self.name, e))
 
-    def _container_cmd_arr(self, cmd, expander: StringVarExpander) -> list:
+    def _container_cmd_arr(self, cmd) -> list:
         info("Preparing container command")
         cmd_array = ["sudo", self.c_tool] if self.c_sudo else [self.c_tool]
         cmd_array.append("exec" if self.c_exec else "run")
         if self.c_cwd:
-            cmd_array += ["-w", expander(self.c_cwd)]
+            cmd_array += ["-w", self.c_cwd]
         if self.c_interactive:
             cmd_array.append("-i")
         if self.c_tty:
@@ -114,17 +138,15 @@ class Task(object):
             if self.c_rm:
                 cmd_array.append("--rm")
             for v in self.c_volumes:
-                cmd_array += ["-v", expander(v)]
+                cmd_array += ["-v", v]
 
         for k, v in self.c_env.items():
-            expanded_k = expander(k)
-            expanded_v = expander(v)
-            cmd_array += ["-e", "{}={}".format(expanded_k, expanded_v)]
-            info("Setting container environment variable will '{}={}'", expanded_k, expanded_v)
+            cmd_array += ["-e", "{}={}".format(k, v)]
+            info("Setting container environment variable will '{}={}'", k, v)
 
         cmd_array += self.c_flags.split()
 
-        cmd_array.append(expander(self.c_image))
+        cmd_array.append(self.c_image)
         if self.c_shell and cmd is not None:
             cmd_array += [self.c_shell_path, "-c"]
         if cmd:
@@ -132,13 +154,13 @@ class Task(object):
         info("Command is {}", cmd_array)
         return cmd_array
 
-    def _run_cmd(self, cmd: list, cmd_str: str, env: typing.Union[dict, None], cwd) -> int:
+    def _run_cmd(self, cmd: list, cmd_str: str) -> int:
         info("Running command (joined):")
         raw_msg(cmd_str)
         p = None
         try:
-            p = subprocess.Popen(cmd, shell=self.shell, executable=self.shell_path, env=env,
-                                 cwd=cwd)
+            p = subprocess.Popen(cmd, shell=self.shell, executable=self.shell_path, env=self.env,
+                                 cwd=self.cwd)
             return p.wait()
 
         except (OSError, FileNotFoundError) as e:
@@ -149,41 +171,26 @@ class Task(object):
                 p.wait()
             raise TaskException("User interrupt")
 
-    def run(self, expander: StringVarExpander) -> int:
-        if self.env is None:
-            info("no special environment variables were set for task")
-            penv = None
-        else:
-
-            penv = os.environ.copy()
-            for k, v in self.env.items():
-                expanded_k = expander(k)
-                expanded_v = expander(v)
-                penv[expanded_k] = expanded_v
-                info("Environment variable will be set '{}={}'", expanded_k, expanded_v)
-        cwd = expander(self.cwd) if self.cwd else None
-        if cwd:
-            info("Working directory will be set to '{}'", cwd)
+    def run(self) -> int:
+        if not self.expanded:
+            raise TaskException("Task must be expanded before run")  # Should never happen
+        if self.cwd:
+            info("Working directory will be set to '{}'", self.cwd)
         else:
             info("No working directory will be set")
 
-        cmds = self.commands
-        if len(cmds) == 0:
+        if len(self.commands) == 0:
             if self.c_image:
                 info("Running container's default command")
-                return self._run_cmd(self._container_cmd_arr(None, expander), "<CONTAINER_DEFAULT>",
-                                     penv, cwd)
+                return self._run_cmd(self._container_cmd_arr(None), "<CONTAINER_DEFAULT>")
             print("No commands defined for task '{}'. Nothing to do.".format(self.name))
             return 0
 
         rc = 0
         for cmd in self.commands:
-            info("Raw command is '{}'", cmd)
-            cmd = expander(cmd)
-            info("Expanded command is '{}'", cmd)
-            cmd_arr = self._container_cmd_arr(cmd, expander) if self.c_image else \
-                self._simple_cmd_arr(cmd)
-            cmd_rc = self._run_cmd(cmd_arr, cmd, penv, cwd)
+            info("Command is '{}'", cmd)
+            cmd_arr = self._container_cmd_arr(cmd) if self.c_image else self._simple_cmd_arr(cmd)
+            cmd_rc = self._run_cmd(cmd_arr, cmd)
             if cmd_rc == 0:
                 continue
             info("Command had failed")
