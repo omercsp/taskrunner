322b9ceda1faecac1e2c08b2382c14559ad1cb1f
Omer Caspi <omer.caspi@gmail.com>
2024-08-03 19:02:52 +0300

    tests: some general love
    Inclding bumping of the container versions
    
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -9,8 +9,8 @@ echo ${OS}
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 if [[ ${OS} == "Linux" ]]; then
-	podman pull docker.io/rockylinux/rockylinux:8.5
-	podman pull docker.io/library/ubuntu:20.04
+	podman pull docker.io/rockylinux/rockylinux:9.3
+	podman pull docker.io/library/ubuntu:24.04
 fi
 
 pip install -e .
